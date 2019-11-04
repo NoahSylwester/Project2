432cb3965f203263f4a199be6630eb1900acac8d
John Weber <johnhenryweber@gmail.com>
2019-11-04 05:20:22 -0800

    Update Rules

---
+++ models/roughRules.js
@@ -0,0 +1,27 @@
+function getTargetCard() {
+  // console.log("Who would you like to target?");
+
+  // User clicks on target creature or player
+  // That object is the return type of the spells
+}
+
+function checkCondition(targetedCard) {
+  if (targetedCard.cardData.defense <= 0){
+    // write discard function
+  }
+}
+
+function targetedSpell(type, value, racial=null) {
+
+  if (type == "directDamage") {
+    targetedCard.cardData.defense -= value;
+    checkCondition(targetedCard);
+  }
+  else if (type == "directHealing") {
+    targetedCard.cardData.defense += value;
+  }
+  else if (type == "increaseBoth"){
+    targetCard.cardData.defense += value;
+    targetCard.cardData.attack += value;
+  }
+}
